131496f1540dd75ec1adc8c4c4fda89c0aca69a1
pauliferrero <159593804+pauliferrero@users.noreply.github.com>
2024-09-17 16:52:49 -0300

    Add files via upload

---
+++ Colectivo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tp2AAT
+{
+    public class Colectivo
+    {
+        public Boleto PagarCon(Tarjeta tarjeta)
+        {
+            if (tarjeta == null)
+                throw new ArgumentNullException(nameof(tarjeta));
+
+            if (!tarjeta.TieneSaldoSuficiente())
+                throw new InvalidOperationException("Saldo insuficiente en la tarjeta.");
+
+            tarjeta.DebitarSaldo();
+            return new Boleto();
+        }
+    }
+}
